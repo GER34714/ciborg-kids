a4eb60e833e973912ac6124a3a7ef90c3118dc4a
ciborg 347 <225079095+GER34714@users.noreply.github.com>
2026-06-24 03:46:22 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// js/config.js
+export const CONFIG = {
+    SUPABASE_URL: 'https://ywchcaonnhwxvozzvzeb.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3Y2hjYW9ubmh3eHZvenp2emViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyNjc2OTksImV4cCI6MjA5Nzg0MzY5OX0.V349aL03N6y7uyeMTpQt9UqHVfmHvHktir3Aht0vYk0',
+    APP_NAME: 'CIBORG KIDS',
+    VERSION: '3.0.0',
+    ADMIN_EMAILS: ['ciborgkids@outlook.com'] // 👈 CAMBIA ESTO POR TU EMAIL
+};
